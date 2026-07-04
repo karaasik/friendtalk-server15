bde726369888d709a6583aa5b1f636c1f5a898bb
karaasik <sergey.agapov2006@yandex.ru>
2026-07-04 17:19:59 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+{
+  "name": "friendtalk-server",
+  "version": "1.0.0",
+  "description": "Backend server for FriendTalk - text, voice and video chat for friends",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "engines": {
+    "node": ">=18.0.0"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "socket.io": "^4.7.5"
+  }
+}
